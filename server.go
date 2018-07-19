8dc78edf939876102ede378862b5cc0dddcca192
INGSOL <inge.solvoll@dnvgl.com>
2018-07-19 16:27:16 +0200

    REST endpoint

--- server.go
+++ server.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 	"github.com/gorilla/mux"
 	"log"
+	"encoding/json"
 )
 
 type PageData struct {
@@ -18,12 +19,23 @@ var data = PageData{
 	CseAnswer: "",
 }
 
+type Simulator struct {
+	ID     string `json:"id,omitempty"`
+	Name   string `json:"name,omitempty"`
+	Status string `json:"status,omitempty"`
+}
+
+func RestTest(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(Simulator{ID: "id-1", Name: "Coral", Status: "Completely broken"})
+}
+
 func Server() {
 	router := mux.NewRouter()
 	tmpl := template.Must(template.ParseFiles("layout.html"))
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl.Execute(w, data)
 	})
+	router.HandleFunc("/rest-test", RestTest)
 	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./resources/public"))))
 	router.HandleFunc("/cse", func(w http.ResponseWriter, r *http.Request) {
 		data.CseAnswer = "The meaning of life is " + strconv.Itoa(cse_hello())
