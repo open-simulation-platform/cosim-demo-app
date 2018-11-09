a512cb7ac406c02098ceb7bdf8a7de9edd9ed852
INGSOL <inge.solvoll@dnvgl.com>
2018-11-09 12:59:43 +0100

    Serving static resources in the right places.

--- server/server.go
+++ server/server.go
@@ -4,32 +4,17 @@ import (
 	"cse-server-go/cse"
 	"cse-server-go/structs"
 	"encoding/json"
-	"html/template"
-	"net/http"
+	"github.com/gobuffalo/packr"
 	"github.com/gorilla/mux"
 	"log"
-	"github.com/gobuffalo/packr"
+	"net/http"
 )
-
-type PageData struct {
-	PageTitle string
-	CseAnswer string
-}
-
-var data = PageData{
-	PageTitle: "CSE Hello World",
-	CseAnswer: "",
-}
 
 func Server(command chan []string, state chan structs.JsonResponse, simulationStatus *structs.SimulationStatus, sim *cse.Simulation) {
 	router := mux.NewRouter()
 	box := packr.NewBox("../resources/public")
-	tmpl := template.Must(template.ParseFiles("../layout.html"))
 
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl.Execute(w, data)
-	})
-
+	router.Handle("/", http.FileServer(box))
 	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(box)))
 
 	router.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
