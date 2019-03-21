1a93ec2a43f91c66ea2dc846bbfe27047d15aeb3
Andreas Flakstad <47635744+flakstad@users.noreply.github.com>
2019-03-21 13:44:45 +0100

    chore(project): add development dependencies for emacs use (#79)

--- project.clj
+++ project.clj
@@ -10,10 +10,11 @@
                  [fulcrologic/semantic-ui-react-wrappers "2.0.4"]
                  [cljsjs/semantic-ui-react "0.84.0-0"]
                  [binaryage/oops "0.7.0"]]
-  :plugins [[lein-figwheel "0.5.16"]
+  :plugins [[lein-figwheel "0.5.18"]
             [figwheel-sidecar "0.5.17"]
             [lein-cljsbuild "1.1.7"]
-            [lein-count "1.0.9"]]
+            [lein-count "1.0.9"]
+            [cider/cider-nrepl "0.21.1"]]
 
   :clean-targets ^{:protect false} [:target-path :compile-path "resources/public/static/js/compiled"]
 
@@ -43,4 +44,7 @@
   :profiles {:dev          [:project/dev :profiles/dev]
              :profiles/dev {}
              :project/dev  {:dependencies [[binaryage/devtools "0.9.10"]
-                                           [day8.re-frame/re-frame-10x "0.3.3-react16"]]}})
+                                           [day8.re-frame/re-frame-10x "0.3.3-react16"]
+                                           [cider/piggieback "0.4.0"]
+                                           [figwheel-sidecar "0.5.18"]]
+                            :repl-options {:nrepl-middelware [cider.piggieback/wrap-cljs-repl]}}})
