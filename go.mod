311e3e089f5e6a82789a230f2aea9501a826ef75
eidekrist <kristoffer.eide@dnvgl.com>
2019-07-10 13:55:29 +0200

    #20 Switch to using msgpack binary format instead of json

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ require (
 	github.com/gobuffalo/packr v1.22.0
 	github.com/gorilla/mux v1.7.0
 	github.com/gorilla/websocket v1.4.0
-	golang.org/x/net v0.0.0-20190213061140-3a22650c66bd
+	github.com/ugorji/go/codec v1.1.7
 )
--- server/websockets.go
+++ server/websockets.go
@@ -2,23 +2,27 @@ package server
 
 import (
 	"cse-server-go/structs"
-	"encoding/json"
 	"github.com/gorilla/websocket"
+	"github.com/ugorji/go/codec"
 	"io"
 	"log"
 	"net/http"
+	"reflect"
 )
 
 type JsonRequest struct {
 	Command     []string `json:"command,omitempty"`
-	Module      string   `json:"module,omitempty"`
-	Modules     bool     `json:"modules,omitempty"`
-	Connections bool     `json:"connections,omitempty"`
 }
 
 var upgrader = websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true },}
 
 func commandLoop(command chan []string, conn *websocket.Conn) {
+	var (
+		mh codec.MsgpackHandle
+	)
+	mh.MapType = reflect.TypeOf(map[string]interface{}(nil))
+	decoder := codec.NewDecoder(nil, &mh)
+
 	for {
 		data := JsonRequest{}
 		_, r, err := conn.NextReader()
@@ -26,7 +30,10 @@ func commandLoop(command chan []string, conn *websocket.Conn) {
 			log.Println("read error:", err)
 			break
 		}
-		err = json.NewDecoder(r).Decode(&data)
+
+		decoder.Reset(r)
+		err = decoder.Decode(&data)
+
 		if err == io.EOF {
 			// One value is expected in the message.
 			log.Println("Message was EOF:", err, data)
@@ -40,11 +47,19 @@ func commandLoop(command chan []string, conn *websocket.Conn) {
 }
 
 func stateLoop(state chan structs.JsonResponse, conn *websocket.Conn) {
+	var (
+		mh codec.MsgpackHandle
+	)
+	mh.MapType = reflect.TypeOf(map[string]interface{}(nil))
+	encoder := codec.NewEncoder(nil, &mh)
 	for {
 		latestState := <-state
-		err := conn.WriteJSON(latestState)
+
+		w, err := conn.NextWriter(2)
+		encoder.Reset(w)
+		err = encoder.Encode(latestState)
 		if err != nil {
-			log.Println("write:", err)
+			log.Println("write error:", err)
 			log.Println("latestState:", latestState)
 			break
 		}
