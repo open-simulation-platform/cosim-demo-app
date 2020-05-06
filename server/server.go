d8782c337720e26cc6a4b4e597840bd9a84a1e80
Flakstad <Andreas.Arning.Flakstad@dnvgl.com>
2020-05-06 13:20:28 +0200

    feature: endpoint for saving current plot configuration
    A new endpoint POST /plot-config is created. Takes the simulation status and writes it in the expected format to
    PlotConfig.json in the directory where the simulation was loaded from. A  preflight endpoint is added to allow cross origin
    calls from figwheel running at port 3449.
    
    Closes #110

--- server/server.go
+++ server/server.go
@@ -25,6 +25,36 @@ func Server(command chan []string, state chan structs.JsonResponse, simulationSt
 		json.NewEncoder(w).Encode(sim.MetaData)
 	})
 
+	router.HandleFunc("/plot-config", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	}).Methods("OPTIONS")
+
+	router.HandleFunc("/plot-config", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		configDir := simulationStatus.ConfigDir
+		trends := simulationStatus.Trends
+		plots := []structs.Plot{}
+		for i := 0; i < len(trends); i++ {
+			trendValues := trends[i].TrendSignals
+ 			variables := []structs.PlotVariable{}
+			for j := 0; j < len(trendValues); j++ {
+				plotVariable := structs.PlotVariable{trendValues[j].Module, trendValues[j].Signal}
+				variables = append(variables, plotVariable)
+			}
+			plot := structs.Plot{trends[i].Label, trends[i].PlotType, variables}
+			plots = append(plots, plot)
+		}
+		plotConfig := structs.PlotConfig{plots}
+		plotConfigJson, _ := json.Marshal(plotConfig)
+		err := ioutil.WriteFile(configDir + "/" + "PlotConfig.json", plotConfigJson, 0644)
+		if err != nil {
+			log.Println("Could not write PlotConfig to file, data: ", plotConfig, ", error was:", err)
+		}
+		json.NewEncoder(w).Encode("Wrote plot configuration to " + configDir + "/" + "PlotConfig.json")
+	}).Methods("POST")
+
 	router.HandleFunc("/value/{module}/{cardinality}/{signal}", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		vars := mux.Vars(r)
