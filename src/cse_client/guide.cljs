b38d845f8726abb53e587f146d9916e0491f6c5d
ljamt <levi.jamt@dnvgl.com>
2020-05-19 19:18:27 +0200

    Adding MPL 2.0 license headers #153 (#154)

--- src/cse_client/guide.cljs
+++ src/cse_client/guide.cljs
@@ -1,3 +1,7 @@
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 (ns cse-client.guide
   (:require [re-frame.core :as rf]
             [cse-client.controller :as controller]))
